4751e1528ed988f5a0231d0685afdf860ec941b4
bunny-k28 <58970641+bunny-k28@users.noreply.github.com>
2021-07-14 16:34:47 +0530

    changed the import statement according to the file containing functions

--- main_UI.py
+++ main_UI.py
@@ -6,20 +6,26 @@
     step 2: Enter the elements of that matrix
     step 3: Enter the operation you want to perform on that matrices
 
-2. All the operations are there in the form of functions in MatrixOperation class.
+2. All the operations are there in the form of M in MatrixOperation class.
 3. The code for that MatrixOperation class is there in a different python file and
     here that class is imported using static import.
 """
 
-from matrix_class import Matrix, menu
+import functions as M
 from termcolor import cprint
 
+
+def drawLine(len=int()):
+    print('-'*len, end='')
+    print('')
+    print('-'*len, end='')
 
 if __name__ == '__main__':
 
     while True:
 
-        print(menu)
+        drawLine(len=50)
+        print(M.menu)
         choice = input('Choose a Operation:- ').lower()
 
         if choice == '1' or choice == 'addition':
@@ -36,28 +42,27 @@ if __name__ == '__main__':
                 print()
 
                 if counterVar1 == 1:
-
-                    mat1 = Matrix().__formMatrix__(rows, cols)
+                    mat1 = M.__formMatrix__(rows, cols)
                     # print(matrix1)
-                    Matrix().__printMatrix__(mat1)
+                    M.__printMatrix__(mat1)
                     print()
 
                 elif counterVar1 == 2:
-                    mat2 = Matrix().__formMatrix__(rows, cols)
+                    mat2 = M.__formMatrix__(rows, cols)
                     # print(matrix2)
-                    Matrix().__printMatrix__(mat2)
+                    M.__printMatrix__(mat2)
                     print()
 
                 counterVar1 += 1
 
-            POM1 = Matrix().__getProperties__(matrix=mat1)
-            POM2 = Matrix().__getProperties__(matrix=mat2)
+            POM1 = M.__getProperties__(matrix=mat1)
+            POM2 = M.__getProperties__(matrix=mat2)
 
             if POM1['order'] == POM2['order']:
 
-                matSum = Matrix().add(matrix1=mat1, matrix2=mat2)
+                matSum = M.add(matrix1=mat1, matrix2=mat2)
                 print('The SUM of your matrices is:- \n')
-                Matrix().__printMatrix__(matrix_2b_print=matSum)
+                M.__printMatrix__(matrix_2b_print=matSum)
 
             else:
                 cprint(text='PropertyError: The order of Matrix1 and Matrix2 are not same!!',
@@ -79,27 +84,27 @@ if __name__ == '__main__':
 
                 if counterVar1 == 1:
 
-                    mat1 = Matrix().__formMatrix__(rows, cols)
+                    mat1 = M.__formMatrix__(rows, cols)
                     # print(matrix1)
-                    Matrix().__printMatrix__(mat1)
+                    M.__printMatrix__(mat1)
                     print()
 
                 elif counterVar1 == 2:
-                    mat2 = Matrix().__formMatrix__(rows, cols)
+                    mat2 = M.__formMatrix__(rows, cols)
                     # print(matrix2)
-                    Matrix().__printMatrix__(mat2)
+                    M.__printMatrix__(mat2)
                     print()
 
                 counterVar1 += 1
 
-            POM1 = Matrix().__getProperties__(matrix=mat1)
-            POM2 = Matrix().__getProperties__(matrix=mat2)
+            POM1 = M.__getProperties__(matrix=mat1)
+            POM2 = M.__getProperties__(matrix=mat2)
 
             if POM1['order'] == POM2['order']:
 
-                matDifference = Matrix().subtract(matrix1=mat1, matrix2=mat2)
+                matDifference = M.subtract(matrix1=mat1, matrix2=mat2)
                 print('The DIFFERENCE of your matrices is:- \n')
-                Matrix().__printMatrix__(matrix_2b_print=matDifference)
+                M.__printMatrix__(matrix_2b_print=matDifference)
 
             else:
                 cprint(text='PropertyError: The order of Matrix1 and Matrix2 are not same!!',
@@ -110,6 +115,8 @@ if __name__ == '__main__':
         elif choice == '3' or choice == 'multiplication':
 
             print('Sorry! The option is Out Of Order')
+            cprint('ATTENTION!!: class is not updated. Install the latest/updated class file to use the'
+                   ' multiplication operator', color='red', attrs='bold')
 
             '''
             print('\n\t\t\t\t\t\t\tMAKE YOUR MATRICES')
@@ -125,27 +132,27 @@ if __name__ == '__main__':
 
                 if counterVar1 == 1:
 
-                    mat1 = Matrix().__formMatrix__(rows, cols)
+                    mat1 = M.__formMatrix__(rows, cols)
                     # print(matrix1)
-                    Matrix().__printMatrix__(mat1)
+                    M.__printMatrix__(mat1)
                     print()
 
                 elif counterVar1 == 2:
-                    mat2 = Matrix().__formMatrix__(rows, cols)
+                    mat2 = M.__formMatrix__(rows, cols)
                     # print(matrix2)
-                    Matrix().__printMatrix__(mat2)
+                    M.__printMatrix__(mat2)
                     print()
 
                 counterVar1 += 1
 
-            POM1 = Matrix().__getProperties__(matrix=mat1)
-            POM2 = Matrix().__getProperties__(matrix=mat2)
+            POM1 = M.__getProperties__(matrix=mat1)
+            POM2 = M.__getProperties__(matrix=mat2)
 
             if POM1['cols'] == POM2['rows']:
 
-                matProduct = Matrix().multiply(matrix1=mat1, matrix2=mat2)
+                matProduct = M.multiply(matrix1=mat1, matrix2=mat2)
                 print('The PRODUCT of your matrices is:- \n')
-                Matrix().__printMatrix__(matrix_2b_print=matProduct)
+                M.__printMatrix__(matrix_2b_print=matProduct)
 
             else:
                 cprint(text='PropertyError: The column of matrix1 != row of matrix2',
@@ -163,18 +170,18 @@ if __name__ == '__main__':
             cols = int(input('Enter the no. columns:- '))
             print()
 
-            Mat = Matrix().__formMatrix__(rows, cols)
+            Mat = M.__formMatrix__(rows, cols)
             # print(matrix1)
-            Matrix().__printMatrix__(Mat)
+            M.__printMatrix__(Mat)
             print()
 
             multiplyInt = int(input('Enter the integer you want to multiply with your matrix:- '))
 
-            scalarMat = Matrix().scalar_multiply(integer=multiplyInt, matrix=Mat)
-            Matrix().__printMatrix__(scalarMat)
-            
+            scalarMat = M.scalar_multiply(integer=multiplyInt, matrix=Mat)
+            M.__printMatrix__(scalarMat)
+
         elif choice == '5' or choice == 'Find Transpose' or choice == 'Transpose':
-            
+
             print('\n\t\t\t\t\t\t\tMAKE YOUR MATRICES')
 
             print(f'Matrix:-\n')
@@ -182,13 +189,13 @@ if __name__ == '__main__':
             cols = int(input('Enter the no. columns:- '))
             print()
 
-            Mat = Matrix().__formMatrix__(rows, cols)
+            Mat = M.__formMatrix__(rows, cols)
             # print(matrix1)
-            Matrix().__printMatrix__(Mat)
+            M.__printMatrix__(Mat)
             print()
 
-            transpose = Matrix().__getTranspose__(matrix=Mat)
-            Matrix().__printMatrix__(transpose)
+            transpose = M.__getTranspose__(matrix=Mat)
+            M.__printMatrix__(transpose)            
 
         else:
             cprint('OutOfMenuError: No such option in menu.', color='red', attrs=['bold'])
